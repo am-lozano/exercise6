ed1a1cd01fb5a3aad8959764341d6b8cf1459d46
am.lozano <am.lozano@106-18.info2>
2024-09-23 14:03:33 +0200

    Exercise6

---
+++ Exercise6/src/exercise6/Exercise6.java
@@ -0,0 +1,12 @@
+package exercise6;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		System.out.println("Antonio Manuel");
+		System.out.println("C/Badajoz\n");
+		System.out.println("655 24 34 47");
+
+	}
+
+}
